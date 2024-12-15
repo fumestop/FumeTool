818631767d58c6b68bea79afb9322f27b1df601a
Sayan Bhattacharyya <sn1f3rt@outlook.com>
2024-12-15 23:53:08 +0530

    docs: add example config

---
+++ config.example.py
@@ -0,0 +1,37 @@
+TOKEN = "token"
+
+EMBED_COLOR = 0xE44C65
+
+WEBHOOK_ID = 1234567890
+WEBHOOK_TOKEN = "webhook_token"
+
+IPC_SECRET_KEY = "ipc_secret_key"
+IPC_STANDARD_PORT = 10003
+IPC_MULTICAST_PORT = 20003
+
+COMMUNITY_GUILD_ID = 1234567890
+
+DB_NAME = "db_name"
+DB_USER = "db_user"
+DB_PASSWORD = "db_password"
+DB_HOST = "localhost"
+DB_PORT = 3306
+
+STEAM_API_KEY = "steam_api_key"
+WEATHER_API_KEY = "weather_api_key"
+
+TOPGG_TOKEN = "topgg_token"
+
+INITIAL_EXTENSIONS = [
+    "cogs.__dev__",
+    "cogs.__error__",
+    "cogs.__eval__",
+    "cogs.__ipc__",
+    "cogs.__topgg__",
+    "cogs.development",
+    "cogs.fun",
+    "cogs.general",
+    "cogs.help",
+    "cogs.tags",
+    "cogs.utility",
+]
